bbcc727ad2bcda1f542ae010ad410f50e3d905ed
Vinit-source <vinitgore1234@gmail.com>
2025-04-12 00:28:50 +0530

    Bug fixes

--- api/gconfig.js
+++ api/gconfig.js
@@ -1,7 +1,6 @@
 // api/gconfig.js
 export default function handler(req, res) {
     const mySecret = process.env.GEMINI_API_KEY;
-    console.log(mySecret)
     if (!mySecret) {
         res.status(500).json({ error: "GEMINI_API_KEY is not set in environment variables" });
     } else {
